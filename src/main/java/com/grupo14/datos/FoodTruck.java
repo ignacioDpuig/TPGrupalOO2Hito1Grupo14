606757bcd0ab7e3602ddcb942c81691a72918b22
Ignacio Puig <ignacio.puig@despegar.com>
2026-08-30 02:42:25 -0300

    feat: agregar HibernateUtil y refactorar FoodTruck y PuestoDesarmable para Hibernate

--- src/main/java/com/grupo14/datos/FoodTruck.java
+++ src/main/java/com/grupo14/datos/FoodTruck.java
@@ -1,57 +1,49 @@
 package com.grupo14.datos;
-import com.grupo14.util.*;
 
 import java.util.List;
 
 public class FoodTruck extends UnidadVenta {
-	private String patente;
-	private boolean requiereConexion;
+    private String patente;
+    private boolean requiereConexion;
 
-	public String getPatente() {
-		return patente;
-	}
+    public FoodTruck() {
+    }
 
-	public boolean getRequiereConexion() {
-		return requiereConexion;
-	}
+    public FoodTruck(int id, String nombreComercial, Personal responsableACargo, float superficie, String codigo,
+                     List<Personal> staff, String patente, boolean requiereConexion,
+                     ConfiguracionCostos configuracionCostos, List<Pedido> pedidos, List<Plato> platos) {
+        super(id, nombreComercial, responsableACargo, superficie, codigo, staff, configuracionCostos, pedidos, platos);
+        this.patente = patente;
+        this.requiereConexion = requiereConexion;
+    }
 
-	public void setPatente(String patente) throws Exception {
-		this.patente = validaciones.validarPatente(patente);
-	}
+    public String getPatente() {
+        return patente;
+    }
 
-	public void setRequiereConexion(boolean requiereConexion) {
-		this.requiereConexion = requiereConexion;
-	}
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
 
-	@Override
-	public double calcularCannon(ConfiguracionCostos configuracionCostos) {
-		float cannon = 0f;
-		cannon += getSuperficie() * configuracionCostos.getCostoSuperficie();
-		if (getRequiereConexion()) {
-			cannon += configuracionCostos.getPlusElectricidad();
-		}
-		return cannon;
-	}
+    public boolean getRequiereConexion() {
+        return requiereConexion;
+    }
 
-	public FoodTruck(int id, String nombreComercial, Personal responsableACargo, float superficie, String codigo,
-			List<Personal> staff, String patente, boolean requiereConexion, ConfiguracionCostos configuracionCostos,
-			List<Pedido> pedidos, List<Plato> platos) throws Exception {
-		super(id, nombreComercial, responsableACargo, superficie, codigo, staff, configuracionCostos, pedidos, platos);
-		setPatente(patente);
-		this.requiereConexion = requiereConexion;
-	}
+    public void setRequiereConexion(boolean requiereConexion) {
+        this.requiereConexion = requiereConexion;
+    }
 
-	public FoodTruck(FoodTruck unidad) throws Exception {
-		super(unidad);
-		setPatente(unidad.getPatente());
-		setRequiereConexion(unidad.getRequiereConexion());
-	}
+    @Override
+    public double calcularCannon(ConfiguracionCostos configuracionCostos) {
+        double cannon = getSuperficie() * configuracionCostos.getCostoSuperficie();
+        if (getRequiereConexion()) {
+            cannon += configuracionCostos.getPlusElectricidad();
+        }
+        return cannon;
+    }
 
-	@Override
-	public String toString() {
-		return "FoodTruck{" +
-				"patente='" + patente + '\'' +
-				", requiereConexion=" + requiereConexion +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "FoodTruck [patente=" + patente + ", requiereConexion=" + requiereConexion + "]";
+    }
 }
--- src/main/java/com/grupo14/datos/PuestoDesarmable.java
+++ src/main/java/com/grupo14/datos/PuestoDesarmable.java
@@ -1,56 +1,49 @@
 package com.grupo14.datos;
-import com.grupo14.util.*;
 
 import java.util.List;
 
 public class PuestoDesarmable extends UnidadVenta {
-	private int cantidadCarpas;
-	private float tiempoMontajeMinutos;
+    private int cantidadCarpas;
+    private float tiempoMontajeMinutos;
 
-	public int getCantidadCarpas() {
-		return cantidadCarpas;
-	}
+    public PuestoDesarmable() {
+    }
 
-	public void setCantidadCarpas(int cantidadCarpas) {
-		this.cantidadCarpas = cantidadCarpas;
-	}
+    public PuestoDesarmable(int id, String nombreComercial, Personal responsableACargo, float superficie, String codigo,
+                            List<Personal> staff, int cantidadCarpas, float tiempoMontajeMinutos,
+                            ConfiguracionCostos configuracionCostos, List<Pedido> pedidos, List<Plato> platos) {
+        super(id, nombreComercial, responsableACargo, superficie, codigo, staff, configuracionCostos, pedidos, platos);
+        this.cantidadCarpas = cantidadCarpas;
+        this.tiempoMontajeMinutos = tiempoMontajeMinutos;
+    }
 
-	public float getTiempoMontajeMinutos() {
-		return tiempoMontajeMinutos;
-	}
+    public int getCantidadCarpas() {
+        return cantidadCarpas;
+    }
 
-	public void setTiempoMontajeMinutos(float tiempoMontajeMinutos) {
-		this.tiempoMontajeMinutos = tiempoMontajeMinutos;
-	}
+    public void setCantidadCarpas(int cantidadCarpas) {
+        this.cantidadCarpas = cantidadCarpas;
+    }
 
-	@Override
-	public double calcularCannon(ConfiguracionCostos configuracionCostos) {
-		float cannon = 0f;
-		cannon += getSuperficie() * configuracionCostos.getCostoSuperficie();
-		cannon -= getTiempoMontajeMinutos() * configuracionCostos.getCostoMontaje();// TODO ESTO DEBERIA SER LOCALDATE
-																					// SACAR MIN CALCULAR EL DECIMAL
-		return cannon;
-	}
+    public float getTiempoMontajeMinutos() {
+        return tiempoMontajeMinutos;
+    }
 
-	public PuestoDesarmable(int id, String nombreComercial, Personal responsableACargo, float superficie, String codigo,
-			List<Personal> staff, int cantidadCarpas, float tiempoMontajeMinutos,
-			ConfiguracionCostos configuracionCostos, List<Pedido> pedidos, List<Plato> platos) throws Exception {
-		super(id, nombreComercial, responsableACargo, superficie, codigo, staff, configuracionCostos, pedidos, platos);
-		this.cantidadCarpas = cantidadCarpas;
-		this.tiempoMontajeMinutos = tiempoMontajeMinutos;
-	}
+    public void setTiempoMontajeMinutos(float tiempoMontajeMinutos) {
+        this.tiempoMontajeMinutos = tiempoMontajeMinutos;
+    }
 
-	public PuestoDesarmable(PuestoDesarmable unidad) throws Exception {
-		super(unidad);
-		setCantidadCarpas(unidad.getCantidadCarpas());
-		setTiempoMontajeMinutos(unidad.getTiempoMontajeMinutos());
-	}
+    @Override
+    public double calcularCannon(ConfiguracionCostos configuracionCostos) {
+        double cannon = getSuperficie() * configuracionCostos.getCostoSuperficie();
+        cannon -= getTiempoMontajeMinutos() * configuracionCostos.getCostoMontaje();
+        // TODO: esto debería usar LocalDate, sacar minutos y calcular el decimal
+        return cannon;
+    }
 
-	@java.lang.Override
-	public java.lang.String toString() {
-		return "PuestoDesarmable{" +
-				"cantidadCarpas=" + cantidadCarpas +
-				", tiempoMontajeMinutos=" + tiempoMontajeMinutos +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "PuestoDesarmable [cantidadCarpas=" + cantidadCarpas +
+               ", tiempoMontajeMinutos=" + tiempoMontajeMinutos + "]";
+    }
 }
